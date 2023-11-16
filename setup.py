56472bf2e07a140c77b22041905656bad1369609
Gian Luca <gianlucap2003@gmail.com>
2023-11-17 00:56:17 +0100

    v1.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='GoogleWalletPassGenerator',
-    version='1.0.1',
+    version='1.0.2',
     license='MIT',
     packages=find_packages(),
     install_requires=[
