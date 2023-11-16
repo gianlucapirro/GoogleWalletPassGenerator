44f34cba78ec4c044a1ee9661d6dc88c8f822f80
Gian Luca <gianlucap2003@gmail.com>
2023-11-16 11:35:51 +0100

    feat: updated setup and setup.cfg

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Google Wallet Pass Generator',
+    name='GoogleWalletPassGenerator',
     version='0.1',
+    license='MIT',
     packages=find_packages(),
     install_requires=[
         'requests',
@@ -13,4 +14,6 @@ setup(
     author='Gian Luca Pirro',
     author_email='gianlucap2003@gmail.com',
     description='A useful package to help you create Google Wallet Passes.',
+    url='https://github.com/gianlucapirro/GoogleWalletPassGenerator',
+    download_url='https://github.com/gianlucapirro/GoogleWalletPassGenerator/archive/refs/tags/GoogleWalletGenerator.tar.gz'
 )
