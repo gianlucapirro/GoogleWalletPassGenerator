4eca3f901de139e80906a03a1b9c760d0e5b921e
Gian Luca <gianlucap2003@gmail.com>
2023-11-16 12:00:40 +0100

    feat: added a pypi description

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='GoogleWalletPassGenerator',
-    version='0.1',
+    version='1.0.1',
     license='MIT',
     packages=find_packages(),
     install_requires=[
