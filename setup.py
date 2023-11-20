80da58a51d762ead2ef8cf7ff81bdd65d6cd06fd
Gian Luca <gianlucap2003@gmail.com>
2023-11-20 12:19:59 +0100

    updated downloadurl

--- setup.py
+++ setup.py
@@ -20,5 +20,5 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/gianlucapirro/GoogleWalletPassGenerator',
-    download_url='https://github.com/gianlucapirro/GoogleWalletPassGenerator/archive/refs/tags/v1.0.2.tar.gz'
+    download_url='https://github.com/gianlucapirro/GoogleWalletPassGenerator/archive/refs/tags/v1.0.3.tar.gz'
 )
